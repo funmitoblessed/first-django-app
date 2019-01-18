e2c6fc1d470c3d0c62dc1c27dff75d0437bf8c10
Oluwafunmito Blessed <funmito.odefemi@outlook.com>
2019-01-18 13:47:02 +0100

    update venv

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@cm^x^8etu2_rjtf#ouy)cq8l%bo+by232e=dxd$9=z#pv-(n^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
